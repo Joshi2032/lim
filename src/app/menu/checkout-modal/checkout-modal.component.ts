50a1f2d571fba81a712fd6a2170abce16eb682d1
Josue Isai Salazar Castro <joshi.jc2032@gmail.com>
2026-01-31 19:55:35 -0600

    fix(checkout-modal): correct CartItem import path

--- src/app/menu/checkout-modal/checkout-modal.component.ts
+++ src/app/menu/checkout-modal/checkout-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CartItem } from '../models/cart.model';
+import { CartItem } from '../../shared/cart/cart.component';
 
 @Component({
   selector: 'app-checkout-modal',
